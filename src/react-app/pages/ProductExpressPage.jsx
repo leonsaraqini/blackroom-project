ea7ca35745a553fd55ad126e8f2ebddf63af865a
leonsaraqini <leonsaraqini@gmail.com>
2026-07-04 10:35:21 +0200

    Update Kairos Express product page

--- src/react-app/pages/ProductExpressPage.jsx
+++ src/react-app/pages/ProductExpressPage.jsx
@@ -1,9 +1,21 @@
+import { useEffect } from 'react'
 import source from '../legacy-pages/product-express.html?raw'
 import ProjectPage from '../components/ProjectPage.jsx'
 import useFreemiusCheckout from '../hooks/useFreemiusCheckout.js'
 
 export default function ProductExpressPage() {
   useFreemiusCheckout()
+
+  useEffect(() => {
+    const root = document.documentElement
+    root.classList.add('brm-product-express-page')
+    root.style.setProperty('--acc', '#ff6a1a')
+
+    return () => {
+      root.classList.remove('brm-product-express-page')
+      root.style.removeProperty('--acc')
+    }
+  }, [])
 
   return (
     <ProjectPage
