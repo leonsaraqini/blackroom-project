449a55fed58e996eeec04e0340da6f352c092c88
leonsaraqini <leonsaraqini@gmail.com>
2026-07-04 10:49:03 +0200

    Serve Kairos Express videos from R2

--- src/react-app/pages/ProductExpressPage.jsx
+++ src/react-app/pages/ProductExpressPage.jsx
@@ -8,12 +8,28 @@ export default function ProductExpressPage() {
 
   useEffect(() => {
     const root = document.documentElement
+    const favicon = document.querySelector('link[rel~="icon"]')
+    const previousFaviconHref = favicon?.getAttribute('href')
+    const previousFaviconType = favicon?.getAttribute('type')
+
     root.classList.add('brm-product-express-page')
     root.style.setProperty('--acc', '#ff6a1a')
+    favicon?.setAttribute('href', '/img/blackroom/shop/express-wordmark.png')
+    favicon?.setAttribute('type', 'image/png')
 
     return () => {
       root.classList.remove('brm-product-express-page')
       root.style.removeProperty('--acc')
+
+      if (favicon && previousFaviconHref) {
+        favicon.setAttribute('href', previousFaviconHref)
+      }
+
+      if (favicon && previousFaviconType) {
+        favicon.setAttribute('type', previousFaviconType)
+      } else {
+        favicon?.removeAttribute('type')
+      }
     }
   }, [])
 
